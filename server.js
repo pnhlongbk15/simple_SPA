26adee6fa3f1c16be5851c7505badbb704fab03a
pnhlongbk15 <101783702+pnhlongbk15@users.noreply.github.com>
2022-08-13 13:40:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use("/static", express.static(path.resolve(__dirname,"frontend","static")));
 app.get("/*", (req,res)=>{
-    res.sendFile(path.resolve("frontend", "app.html"));
+    res.sendFile(path.resolve("frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 5000, ()=> console.log("Server running...")); 
